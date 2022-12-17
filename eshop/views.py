ae08e25b7d638dd532111c267566e73a60335c9b
DAFE <dafecoulibaly97@gmail.com>
2022-12-17 10:30:19 +0000

    config and add template in eshop

---
+++ eshop/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"eshop/index.html", {})
+
+def shop(request):
+    return render(request,"eshop/shop.html", {})
+
+def detail(request):
+    return render(request,"eshop/detail.html", {})
+
+def contact(request):
+    return render(request,"eshop/contact.html", {})
+
+def cart(request):
+    return render(request,"eshop/cart.html", {})
+
+def checkout(request):
+    return render(request,"eshop/checkout.html", {})
