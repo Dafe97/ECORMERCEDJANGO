cd8855911efd84f9d49df789b97daf61b8ba410a
DAFE <dafecoulibaly97@gmail.com>
2023-01-07 12:02:14 +0000

    add model category

--- eshop/models.py
+++ eshop/models.py
@@ -17,3 +17,10 @@ class Image(models.Model):
 
     def __str__(self):
         return f"{self.id} :{self.name}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=45, null=False, blank=False, unique=True)
+    image = models.ImageField(null= True, blank=True, upload_to="Images/categories/")
+    parent = models.ForeignKey('Category',null=True, related_name="subcategories",on_delete=models.SET_NULL)
+    def __str__(self) -> str:
+        return f" {self.id}: {self.name}"
