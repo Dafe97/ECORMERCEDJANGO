b37069beca0db01df5b5cc565d51055bd90691ec
DAFE <dafecoulibaly97@gmail.com>
2023-01-07 11:31:03 +0000

    add all page navigate

--- eshop/models.py
+++ eshop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, unique=True )
+    description = models.TextField(max_length=50,null=True, blank=True )
+    price = models.FloatField(null=True, blank=True)
+    stock = models.IntegerField(default=0, null=True, blank=True )
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True )
+
+    def __str__(self):
+        return f"{self.id} :{self.name}"
+
+class Image(models.Model):
+    name = models.ImageField(null=True, blank=True, upload_to='products/images')
+    products = models.ForeignKey('Product',on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        return f"{self.id} :{self.name}"
