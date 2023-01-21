6aaa948deed0d156260d4b96352b5796cf12afe1
DAFE <dafecoulibaly97@gmail.com>
2023-01-21 13:02:37 +0000

    connect product in detail page

--- eshop/models.py
+++ eshop/models.py
@@ -6,13 +6,22 @@ class Product(models.Model):
     description = models.TextField(max_length=50,null=True, blank=True )
     price = models.FloatField(null=True, blank=True)
     stock = models.IntegerField(default=0, null=True, blank=True )
+    Category = models.ForeignKey('Category',null=True,blank=False,on_delete=models.SET_NULL,related_name='products')
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True )
 
     def __str__(self):
         return f"{self.id} :{self.name}"
+    
+    @property
+    def first_image(self):
+        if self.images.all():
+            return self.images.all()[0].name.url
+        else:
+            return ''
+            
 
 class Image(models.Model):
-    name = models.ImageField(null=True, blank=True, upload_to='products/images')
+    name = models.ImageField(null=True, blank=True, upload_to='images/products')
     products = models.ForeignKey('Product',on_delete=models.CASCADE, related_name='images')
 
     def __str__(self):
@@ -20,7 +29,7 @@ class Image(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=45, null=False, blank=False, unique=True)
-    image = models.ImageField(null= True, blank=True, upload_to="Images/categories/")
-    parent = models.ForeignKey('Category',null=True, related_name="subcategories",on_delete=models.SET_NULL)
+    image = models.ImageField(null= True, blank=True, upload_to="images/categories/")
+    parent = models.ForeignKey('Category',null=True, related_name="subcategories",on_delete=models.SET_NULL,blank=True)
     def __str__(self) -> str:
         return f" {self.id}: {self.name}"
---
+++ java
@@ -0,0 +1,10 @@
+{"A001", "Bamane",    "Djibril",    "Bamako",  "77 14 25 45", "GL3"}
+{"A002", "Cissé",     "Djeneba",    "Sikasso", "65 89 25 36", "GL3"},
+{"A003", "Dembélé",   "Y Japhet",   "Bamako",  "66 25 89 36", "GL3"},
+{"A004", "Diakité",   "Bakary",     "Sikasso", "74 56 89 11", "GL3"},
+{"A005", "Diarra",    "Sidi L",     "Gao",     "75 58 66 33", "GL4"},
+{"B001", "Diarra",    "Maïmouna",   "Sikasso", "68 25 89 77", "GL4"},
+{"B002", "Kamissoko", "Sékou",      "Bamako",  "67 55 12 33", "GL4"},
+{"B003", "Keita",     "Sira",       "Sikasso", "71 25 66 33", "GL5"},
+{"B004", "Konaré",    "Yoro",       "Ségou",   "63 25 22 33", "GL5"},
+{"B005", "Koné",      "Gaoussou" ,  "Bamako",  "70 12 33 56", "GL5"},
